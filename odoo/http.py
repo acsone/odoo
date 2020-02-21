63c3c7b55613c9034c1da5567533fc736b77eb7e
Xavier Morel <xmo@odoo.com>
2020-02-21 12:32:38 +0000

    [FIX] core: modules moved or removed in Werkzeug 1.0
    Those were deprecations implemented in 0.15
    
    * all middlewares have been moved from `werkzeug.wsgi` to
      `werkzeug.middleware`, including the `SharedDataMiddleware` we use
    * ProxyFix was moved to werkzeug.middleware.proxy_fix, this had
      already been fixed but I forgot the import
    * sessions support was moved to a separate package
      (`pallets/secure-cookies`), however while distros are starting to
      update werkzeug to 1.0 (e.g. done on Arch, and in Debian
      Experimental) they're not bundling secure-cookies so using a
      vendored version seems like the least bad thing we can do, even more
      so as conditional dependencies are not really a thing (e.g. even
      with just pip we can't depend on secure-cookie iff werkzeug >= 1.0)
    
    (cherry picked from commit c58c61883389d10b58837189c03642dc8becec39)

--- odoo/http.py
+++ odoo/http.py
@@ -55,7 +55,6 @@ from .tools.func import lazy_property
 from .tools import ustr, consteq, frozendict, pycompat, unique, date_utils
 from .tools.mimetypes import guess_mimetype
 from .tools._vendor import sessions
-from .tools._vendor.useragents import UserAgent
 from .modules.module import module_manifest
 
 
