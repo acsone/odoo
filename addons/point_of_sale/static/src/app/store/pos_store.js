96dbc401dc0ae84632092b8a0c39e913ee4737f8
Pedram (PEBR) <pebr@odoo.com>
2025-12-18 10:59:06 +0100

    [FIX] point_of_sale: allow pos manager to create products from POS
    Before this commit, only system users could create products from the POS
    interface. This limited the ability of store managers.
    
    opw-5418727
    
    closes odoo/odoo#240520
    
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/store/pos_store.js
+++ addons/point_of_sale/static/src/app/store/pos_store.js
@@ -1942,7 +1942,7 @@ export class PosStore extends Reactive {
         );
     }
     async allowProductCreation() {
-        return await user.hasGroup("base.group_system");
+        return await user.checkAccessRight("product.product", "create");
     }
     orderDetailsProps(order) {
         return {
